c1aa81adb1462050804fdc599c3824afc27a9698
dylman123 <dylanklein29@gmail.com>
2022-11-17 13:14:15 +1100

    retrieved publicToken via frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+import FlexpaLink from '@flexpa/link'
 
 export default function Home() {
+
+  useEffect(() => {
+    // Run on client-side only
+    FlexpaLink.create({
+      // Replace with your publishable key
+      publishableKey: 'pk_test_DPfJED99WVLf1mNAQOCsf_iGxuQh3668qnAEc6FScEA', 
+      onSuccess: (publicToken) => {
+        // Send `publicToken` to your backend to exchange it for a patient `access_token`
+        // https://www.flexpa.com/docs/sdk/login#exchange
+        console.log('publicToken: ', publicToken)
+      }
+    });
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,25 +38,26 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <button onClick={() => FlexpaLink.open()}>Link your health data</button>
+          {/* <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </a> */}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          {/* <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -50,7 +67,7 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
