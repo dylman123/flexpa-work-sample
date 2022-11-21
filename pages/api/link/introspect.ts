1da8e2c59022bd55d135de94532e69871bda0ba2
dylman123 <dylanklein29@gmail.com>
2022-11-21 17:19:20 +1100

    centralised type: 'Data'

--- pages/api/link/introspect.ts
+++ pages/api/link/introspect.ts
@@ -1,9 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  data: string
-}
+import { Data } from '../../../domain/models'
 
 export default async function handler(
   req: NextApiRequest,
